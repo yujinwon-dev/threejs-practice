08f48b33be222f66ae6ae70a7957d1c61a3183cd
yujin won <won_yujin@naver.com>
2022-04-14 10:01:33 +0900

    feat: 🎸 webpack 실습 추가

---
+++ webpack-js-html/src/TextSign.js
@@ -0,0 +1,14 @@
+// export default class TextSign {
+// 	constructor() {
+// 		this.elem = document.createElement('div');
+// 		this.elem.classList.add('text-sign');
+// 		this.elem.style.transform = `translate(${Math.random() * window.innerWidth * 0.8}px, ${Math.random() * window.innerHeight * 0.8}px)`;
+// 		document.body.appendChild(this.elem);
+// 	}
+// }
+
+export function message(msg) {
+  const elem = document.createElement("p");
+  elem.innerHTML = msg;
+  document.body.append(elem);
+}
